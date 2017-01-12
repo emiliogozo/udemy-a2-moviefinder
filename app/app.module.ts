4fb8f4a87965c68d26b8eaec0475fbe5c95502bc
Emilio Gozo <emilio.gozo@obf.ateneo.edu>
2017-01-12 15:04:50 +0800

    feat: Router and Movie details

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,20 +2,24 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule, JsonpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
+import { routing } from './app.routing';
 
 import { AppComponent }  from './app.component';
 import { MoviesComponent } from './components/movies/movies.component';
+import { MovieComponent } from './components/movie/movie.component';
 
 @NgModule({
   imports:      [
     BrowserModule,
     HttpModule,
     JsonpModule,
-    FormsModule
+    FormsModule,
+    routing
   ],
   declarations: [
     AppComponent,
-    MoviesComponent
+    MoviesComponent,
+    MovieComponent
   ],
   bootstrap:    [AppComponent]
 })
